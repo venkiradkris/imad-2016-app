333b185af2017155f6fe0826752fb5b25ece1f6b
venkiradkris <venkiradkris@gmail.com>
2016-10-05 18:46:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,39 +6,45 @@ var app = express();
 app.use(morgan('combined'));
 
 
-var content = {
+var articleOneObj = {
     title: 'Article One | Venkatesh Radhakrishnan',
     heading: 'Article One',
     date: 'Oct 5 2016',
     content: '<P>THIS IS MY FIRST ARTICLE | THIS IS MY FIRST ARTICLE | THIS IS MY FIRST ARTICLE</P><P>THIS IS MY FIRST ARTICLE | THIS IS MY FIRST ARTICLE</P><P>THIS IS MY FIRST ARTICLE</P>'
 };
 
-var htmltemplate = `
-<html>
-<head>
-    <title>${title}</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <link href="/ui/style.css" rel="stylesheet" />
-</head>
-<body>
-    <div class="container">
-            <div><a href="/">Home</a></div>
-            <hr/>
-            <h3>${heading}</h3>
-            <div>${date}</div>
-            <div>${content}</div>
-    </div>
-</body>
-</html>
-`;
-
+function createTemplate (data){
+    var title = data.title;  
+    var date = data.date;  
+    var heading = data.heading;  
+    var content = data.content;  
+    var htmltemplate = `
+    <html>
+    <head>
+        <title>${title}</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+    <body>
+        <div class="container">
+                <div><a href="/">Home</a></div>
+                <hr/>
+                <h3>${heading}</h3>
+                <div>${date}</div>
+                <div>${content}</div>
+        </div>
+    </body>
+    </html>
+    `;
+    return htmltemplate;
+}
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article-one', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+  res.send(createTemplate(articleOneObj));
 });
 
 app.get('/article-two', function (req, res) {
