d83c7444ca501c0d607e9907f6a40a91e5a34321
venkiradkris <venkiradkris@gmail.com>
2016-10-05 18:39:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,6 +13,26 @@ var content = {
     content: '<P>THIS IS MY FIRST ARTICLE | THIS IS MY FIRST ARTICLE | THIS IS MY FIRST ARTICLE</P><P>THIS IS MY FIRST ARTICLE | THIS IS MY FIRST ARTICLE</P><P>THIS IS MY FIRST ARTICLE</P>'
 };
 
+var htmltemplate = `
+<html>
+<head>
+    <title>${title}</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link href="/ui/style.css" rel="stylesheet" />
+</head>
+<body>
+    <div class="container">
+            <div><a href="/">Home</a></div>
+            <hr/>
+            <h3>${heading}</h3>
+            <div>${date}</div>
+            <div>${content}</div>
+    </div>
+</body>
+</html>
+`;
+
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
