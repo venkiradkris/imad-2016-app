4c464cfb819ce8532d72236e4c92d5fbad8dd117
venkiradkris <venkiradkris@gmail.com>
2016-10-06 12:53:31 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -20,11 +20,26 @@ var nameInput = document.getElementById('name');
 var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick = function () {
-    var names = ['name 1', 'name 2', 'name 3', 'name 4'];
-    var list = '';
-    for (var i=0; i<names.length; i++){
-        list += '<li>' + names[i] + '</li>';
-    }
-    var ul = document.getElementById('namelist');    
-    ul.innerHTML = list;
+    
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function () {
+        if (request.readystate === XMLHttpRequest.DONE) {
+            if (request.status === 200) {
+    
+    
+                //var names = ['name 1', 'name 2', 'name 3', 'name 4'];
+                var names = request.ResponseText;
+                names = JSON.parse(names);
+                var list = '';
+                for (var i=0; i<names.length; i++){
+                    list += '<li>' + names[i] + '</li>';
+                }
+                var ul = document.getElementById('namelist');    
+                ul.innerHTML = list;
+            }
+        }
+    };
+                
+    request.open('GET', 'http://venkiradkris.imad.hasura-app.io/submit-name/' +name, true);
+    request.send(null);
 };
