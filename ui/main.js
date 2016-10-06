8f28fccd066714a9ac6e3ad07f3bd376b28e2315
venkiradkris <venkiradkris@gmail.com>
2016-10-06 12:59:49 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -39,6 +39,6 @@ submit.onclick = function () {
     };
     var nameInput = document.getElementById('name');
     var name = nameInput.value;
-    request.open('GET', 'http://venkiradkris.imad.hasura-app.io/submit-name/' +name, true);
+    request.open('GET', 'http://venkiradkris.imad.hasura-app.io/submit-name?name=' +name, true);
     request.send(null);
 };
