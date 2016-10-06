7f01690ca02b10cd1766cafb2d617c580e0b0276
venkiradkris <venkiradkris@gmail.com>
2016-10-06 12:35:21 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -15,3 +15,16 @@ button.onclick = function () {
     request.open('GET', 'http://venkiradkris.imad.hasura-app.io/counter', true);
     request.send(null);
 };
+
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
+var submit = document.getElementById('submit_btn');
+submit.onclick = function () {
+    var names = ['name 1', 'name 2', 'name 3', 'name 4'];
+    var list = '';
+    for (var i=0; i<names.length; i++){
+        list += '<li>' + names[i] + '</li>';
+    }
+    var ul = document.getElementById('namelist');    
+    ul.innerHTML = list;
+}
