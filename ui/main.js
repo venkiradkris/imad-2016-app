50772da4c8382a48837fc46050d12df892a977b6
venkiradkris <venkiradkris@gmail.com>
2016-10-06 12:08:41 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,32 +1,24 @@
-console.log('Loaded!');
+//var button = document.getElementById('counter');
+//var counter = 0;
+//button.onclick = function () {
+    //counter = counter + 1;
+    //var span = document.getElementById("count");
+    //span.innerHTML = counter.toString();
+//};
 
 var button = document.getElementById('counter');
-
 var counter = 0;
-
 button.onclick = function () {
-    
-    
-    //var request = new XMLHttpRequest();
-    
-    //request.onreadystatechange = function () {
-        
-        //if (request.readystate === XMLHttpRequest.DONE) {
-            
-            //if (request.status === 200) {
-                //var counter = request.responseText;
-                //var span = document.getElementById('count');
-                //alert(counter.toString());
-                //span.innerHTML = counter.toString();
-            //}
-        //}
-    //};
-    
-    //request.open('GET', 'http://venkiradkris.imad.hasura-app.io/counter', true);
-    //request.send(null);
-
-    counter = counter + 1;
-    var span = document.getElementById("count");
-    span.innerHTML = counter.toString();
-    
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function () {
+        if (request.readystate === XMLHttpRequest.DONE) {
+            if (request.status === 200) {
+                var counter = request.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString();
+            }
+        }
+    };
+    request.open('GET', 'http://venkiradkris.imad.hasura-app.io/counter', true);
+    request.send(null);
 };
