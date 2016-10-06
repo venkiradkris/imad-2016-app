a2367792c4b04ef8df9955076ef40b193da39b98
venkiradkris <venkiradkris@gmail.com>
2016-10-06 12:49:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -49,7 +49,7 @@ app.get('/counter', function (req, res) {
 });
 
 var names = [];
-app.get('/submit-name', function (req, res) {
+app.get('/submit-name/:name', function (req, res) {
   var name = req.params.name;
   names.push(name);
   //JSON JavaScript Object Notation
