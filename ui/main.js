2cbbd63b77b500dca058ae0d58cd01875fb30980
venkiradkris <venkiradkris@gmail.com>
2016-10-06 12:02:10 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -15,6 +15,7 @@ button.onclick = function () {
             if (request.status === 200) {
                 var counter = request.responseText;
                 var span = document.getElementById('count');
+                alert(counter.toString());
                 span.innerHTML = counter.toString();
             }
         }
